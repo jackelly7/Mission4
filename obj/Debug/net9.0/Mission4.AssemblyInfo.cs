8aac7d7a071cf7f6c851b81f03a58e4efc846012
ethnhdgns <ethanhudgins@outlook.com>
2025-01-29 09:57:34 -0700

    ethans

--- obj/Debug/net9.0/Mission4.AssemblyInfo.cs
+++ obj/Debug/net9.0/Mission4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8714187a10b66fa691ee3845825fd367b8aa345c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
